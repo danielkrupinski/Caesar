bba940363c2d5c18c9492752a25e81030e73529e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-01 10:45:29 +0200

    Remove std::string base64_encode(const std::string &in) definition

--- Caesar/Main.cpp
+++ Caesar/Main.cpp
@@ -54,28 +54,6 @@ LRESULT CALLBACK Hooked_WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 	return CallWindowProc(g_pGlobals.WndProcBackup, hwnd, uMsg, wParam, lParam);
 }
 
-/*
-
-std::string base64_encode(const std::string &in)
-{
-	std::string out;
-
-	int val = 0, valb = -6;
-	for (unsigned char c : in) {
-		val = (val << 8) + c;
-		valb += 8;
-		while (valb >= 0) {
-			out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val >> valb) & 0x3F]);
-			valb -= 6;
-		}
-	}
-	if (valb>-6) out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val << 8) >> (valb + 8)) & 0x3F]);
-	while (out.size() % 4) out.push_back('=');
-	return out;
-}
-
-*/
-
 static const std::string base64_chars =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 "abcdefghijklmnopqrstuvwxyz"
