6482e9fd98748facad1ad6f834926a348d44a0ed
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-04 20:11:47 +0200

    Do not call Config::init() in Config constructor

--- Caesar/Config.cpp
+++ Caesar/Config.cpp
@@ -14,7 +14,7 @@ Config::Config(const std::string& name)
     if (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, 0, buff))) {
         path = buff + std::string{ '\\' } + name;
     }
-    init();
+    // init();
 }
 
 /*
