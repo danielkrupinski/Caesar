c82da7c8339b7b404861a3709de9e1c00880a8b3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-06 19:06:31 +0200

    Remove old iniPath initialization in DLLMain

--- Caesar/Main.cpp
+++ Caesar/Main.cpp
@@ -27,129 +27,122 @@ VHookTable PanelHook;
 
 LRESULT CALLBACK Hooked_WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-	case WM_LBUTTONDOWN:
-		g_Menu.keys[VK_LBUTTON] = true;
-		break;
-	case WM_LBUTTONUP:
-		g_Menu.keys[VK_LBUTTON] = false;
-		break;
-	case WM_RBUTTONDOWN:
-		g_Menu.keys[VK_RBUTTON] = true;
-		break;
-	case WM_RBUTTONUP:
-		g_Menu.keys[VK_RBUTTON] = false;
-		break;
-	case WM_KEYDOWN:
-		g_Menu.keys[wParam] = true;
-		break;
-	case WM_KEYUP:
-		g_Menu.keys[wParam] = false;
-		break;
-	default: break;
-	}
+    switch (uMsg)
+    {
+    case WM_LBUTTONDOWN:
+        g_Menu.keys[VK_LBUTTON] = true;
+        break;
+    case WM_LBUTTONUP:
+        g_Menu.keys[VK_LBUTTON] = false;
+        break;
+    case WM_RBUTTONDOWN:
+        g_Menu.keys[VK_RBUTTON] = true;
+        break;
+    case WM_RBUTTONUP:
+        g_Menu.keys[VK_RBUTTON] = false;
+        break;
+    case WM_KEYDOWN:
+        g_Menu.keys[wParam] = true;
+        break;
+    case WM_KEYUP:
+        g_Menu.keys[wParam] = false;
+        break;
+    default: break;
+    }
 
-	return CallWindowProc(g_pGlobals.WndProcBackup, hwnd, uMsg, wParam, lParam);
+    return CallWindowProc(g_pGlobals.WndProcBackup, hwnd, uMsg, wParam, lParam);
 }
 
 DWORD WINAPI Hook(LPVOID lpThreadParameter)
 {
-	CreateInterfaceFn gameui_factory = CaptureFactory("gameui.dll");
-	CreateInterfaceFn vgui2_factory = CaptureFactory("vgui2.dll");
-	CreateInterfaceFn hardware_factory = CaptureFactory("hw.dll");
-	CreateInterfaceFn client_factory = CaptureFactory("client.dll");
+    CreateInterfaceFn gameui_factory = CaptureFactory("gameui.dll");
+    CreateInterfaceFn vgui2_factory = CaptureFactory("vgui2.dll");
+    CreateInterfaceFn hardware_factory = CaptureFactory("hw.dll");
+    CreateInterfaceFn client_factory = CaptureFactory("client.dll");
 
-	if (gameui_factory && vgui2_factory && hardware_factory && client_factory)
-	{
-		VIRTUALIZER_START
-		while (!g_Offsets.FindHardware())
-			Sleep(100);
+    if (gameui_factory && vgui2_factory && hardware_factory && client_factory)
+    {
+        VIRTUALIZER_START
+            while (!g_Offsets.FindHardware())
+                Sleep(100);
 
-		g_pIVGuiModuleLoader = (IVGuiModuleLoader*)(CaptureInterface(gameui_factory, VGUIMODULELOADER_INTERFACE_VERSION));
-		g_pIRunGameEngine = (IRunGameEngine*)(CaptureInterface(gameui_factory, RUNGAMEENGINE_INTERFACE_VERSION));
-		g_pGameUI = (IGameUI*)(CaptureInterface(gameui_factory, GAMEUI_INTERFACE_VERSION));
-		g_pConsole = (IGameConsole*)(CaptureInterface(gameui_factory, GAMECONSOLE_INTERFACE_VERSION));
-		g_pIPanel = (vgui::IPanel*)(CaptureInterface(vgui2_factory, VGUI_PANEL_INTERFACE_VERSION));
-		g_pISurface = (vgui::ISurface*)(CaptureInterface(hardware_factory, VGUI_SURFACE_INTERFACE_VERSION));
-		g_pIEngineVGui = (vgui::IEngineVGui*)(CaptureInterface(hardware_factory, VENGINE_VGUI_VERSION));
-		g_pClient = (cl_clientfunc_t*)g_Offsets.FindClient();
-		g_pEngine = (cl_enginefunc_t*)g_Offsets.FindEngine();
-		g_pStudio = (engine_studio_api_t*)g_Offsets.FindStudio();
-		g_pStudioModelRenderer = (StudioModelRenderer_t*)g_Offsets.FindStudioModelRenderer();
-		
-		while (!g_Client.V_CalcRefdef)
-			RtlCopyMemory(&g_Client, g_pClient, sizeof(cl_clientfunc_t));
+        g_pIVGuiModuleLoader = (IVGuiModuleLoader*)(CaptureInterface(gameui_factory, VGUIMODULELOADER_INTERFACE_VERSION));
+        g_pIRunGameEngine = (IRunGameEngine*)(CaptureInterface(gameui_factory, RUNGAMEENGINE_INTERFACE_VERSION));
+        g_pGameUI = (IGameUI*)(CaptureInterface(gameui_factory, GAMEUI_INTERFACE_VERSION));
+        g_pConsole = (IGameConsole*)(CaptureInterface(gameui_factory, GAMECONSOLE_INTERFACE_VERSION));
+        g_pIPanel = (vgui::IPanel*)(CaptureInterface(vgui2_factory, VGUI_PANEL_INTERFACE_VERSION));
+        g_pISurface = (vgui::ISurface*)(CaptureInterface(hardware_factory, VGUI_SURFACE_INTERFACE_VERSION));
+        g_pIEngineVGui = (vgui::IEngineVGui*)(CaptureInterface(hardware_factory, VENGINE_VGUI_VERSION));
+        g_pClient = (cl_clientfunc_t*)g_Offsets.FindClient();
+        g_pEngine = (cl_enginefunc_t*)g_Offsets.FindEngine();
+        g_pStudio = (engine_studio_api_t*)g_Offsets.FindStudio();
+        g_pStudioModelRenderer = (StudioModelRenderer_t*)g_Offsets.FindStudioModelRenderer();
 
-		while (!g_Engine.V_CalcShake)
-			RtlCopyMemory(&g_Engine, g_pEngine, sizeof(cl_enginefunc_t));
+        while (!g_Client.V_CalcRefdef)
+            RtlCopyMemory(&g_Client, g_pClient, sizeof(cl_clientfunc_t));
 
-		while (!g_Studio.StudioSetupSkin)
-			RtlCopyMemory(&g_Studio, g_pStudio, sizeof(engine_studio_api_t));
+        while (!g_Engine.V_CalcShake)
+            RtlCopyMemory(&g_Engine, g_pEngine, sizeof(cl_enginefunc_t));
 
-		while (!g_StudioModelRenderer.StudioSlerpBones)
-			RtlCopyMemory(&g_StudioModelRenderer, g_pStudioModelRenderer, sizeof(StudioModelRenderer_t));
+        while (!g_Studio.StudioSetupSkin)
+            RtlCopyMemory(&g_Studio, g_pStudio, sizeof(engine_studio_api_t));
 
-		while (!pmove)
-			pmove = (playermove_t*)g_Offsets.FindPlayerMove();
+        while (!g_StudioModelRenderer.StudioSlerpBones)
+            RtlCopyMemory(&g_StudioModelRenderer, g_pStudioModelRenderer, sizeof(StudioModelRenderer_t));
 
-		while (!pUserMsgBase)
-			pUserMsgBase = g_Offsets.FindUserMsgBase();
+        while (!pmove)
+            pmove = (playermove_t*)g_Offsets.FindPlayerMove();
 
-		g_Drawing.SetupFonts();
+        while (!pUserMsgBase)
+            pUserMsgBase = g_Offsets.FindUserMsgBase();
 
-		if (PanelHook.HookTable((DWORD)g_pIPanel))
-		{
-			PanelHook.HookIndex(41, PaintTraversePanel);
-		}
+        g_Drawing.SetupFonts();
 
-		g_NoFlash.pScreenFade = *(screenfade_t**)((DWORD)g_Engine.pfnSetScreenFade + 0x17);
+        if (PanelHook.HookTable((DWORD)g_pIPanel))
+        {
+            PanelHook.HookIndex(41, PaintTraversePanel);
+        }
 
-		if (IsBadReadPtr(g_NoFlash.pScreenFade, sizeof(screenfade_t)))
-			g_NoFlash.pScreenFade = *(screenfade_t**)((DWORD)g_Engine.pfnSetScreenFade + 0x18);
+        g_NoFlash.pScreenFade = *(screenfade_t**)((DWORD)g_Engine.pfnSetScreenFade + 0x17);
 
-		g_Offsets.GlobalTime();
+        if (IsBadReadPtr(g_NoFlash.pScreenFade, sizeof(screenfade_t)))
+            g_NoFlash.pScreenFade = *(screenfade_t**)((DWORD)g_Engine.pfnSetScreenFade + 0x18);
 
-		g_Offsets.dwSpeedPointer = (DWORD)g_Offsets.FindSpeed();
+        g_Offsets.GlobalTime();
 
-		while (!g_pGlobals.hWindow)
-		{
-			if (!(g_pGlobals.hWindow = FindWindowA("Valve001", NULL)))
-				g_pGlobals.hWindow = FindWindowA(NULL, "Counter-Strike");
+        g_Offsets.dwSpeedPointer = (DWORD)g_Offsets.FindSpeed();
 
-			Sleep(200);
-		}
+        while (!g_pGlobals.hWindow)
+        {
+            if (!(g_pGlobals.hWindow = FindWindowA("Valve001", NULL)))
+                g_pGlobals.hWindow = FindWindowA(NULL, "Counter-Strike");
+
+            Sleep(200);
+        }
 
         // g_pGlobals.IniPath = "%USERPROFILE%\Documents";
 
-		g_pGlobals.WndProcBackup = (WNDPROC)SetWindowLongA(g_pGlobals.hWindow, GWL_WNDPROC, (LONG_PTR)&Hooked_WndProc);
+        g_pGlobals.WndProcBackup = (WNDPROC)SetWindowLongA(g_pGlobals.hWindow, GWL_WNDPROC, (LONG_PTR)&Hooked_WndProc);
 
-		HookClient();
-		VIRTUALIZER_END
-	}
+        HookClient();
+        VIRTUALIZER_END
+    }
 
-	return NULL;
+    return NULL;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
 {
-	if (dwReason == DLL_PROCESS_ATTACH)
-	{
-		DisableThreadLibraryCalls(hinstDLL);
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hinstDLL);
 
-		if (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, 0, g_pGlobals.IniPath)))
-		{
-            strcat(g_pGlobals.IniPath, "\\Caesar.ini");
-            // g_pGlobals.BaseDir = baseDir;
-            // g_pGlobals.BaseDir = g_pGlobals.BaseDir.substr(0, g_pGlobals.BaseDir.find_last_of('\\') + 1);
+        GetModuleFileNameA(NULL, g_pGlobals.GamePath, MAX_PATH);
 
-			GetModuleFileNameA(NULL, g_pGlobals.GamePath, MAX_PATH);
+        char* pos = g_pGlobals.GamePath + strlen(g_pGlobals.GamePath);
+        while (pos >= g_pGlobals.GamePath && *pos != '\\') --pos; pos[1] = 0;
 
-			char* pos = g_pGlobals.GamePath + strlen(g_pGlobals.GamePath);
-			while (pos >= g_pGlobals.GamePath && *pos != '\\') --pos; pos[1] = 0;
- 
-			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Hook, NULL, NULL, NULL);
-		}
-	}
-	return TRUE;
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Hook, NULL, NULL, NULL);
+    }
+    return TRUE;
 }
