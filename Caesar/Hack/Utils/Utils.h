c2aa0cd04ff735f37ef0622eb01333eee780ec88
Daniel Krupiński <danielkrupinski@outlook.com>
2018-09-07 17:38:35 +0200

    Remove void CUtils::cIniWrite()

--- Caesar/Hack/Utils/Utils.h
+++ Caesar/Hack/Utils/Utils.h
@@ -19,7 +19,6 @@ public:
 	bool bFileExists(const char *cFileName);
 	char *szDirFile(const char* pszName);
 	char *cIniRead(char *cFileName, char *cSection, char *cKey, char *cDef);
-	// void cIniWrite(char *cFileName, char *cSection, char *cKey, char *cValue);
 
 	bool bCalcScreen(float *pflOrigin, float *pflVecScreen);
 	bool bPathFree(float *pflFrom, float *pflTo);
